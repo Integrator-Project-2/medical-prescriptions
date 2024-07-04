45fd3c31525ee7b4960de5e6f09cc69a8b03e683
KarlaJulyana <karla@solarz.com.br>
2024-07-04 15:49:49 -0300

    chore: create model medical prescriptions #5

--- prescriptions/models.py
+++ prescriptions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MedicalPrescription(models.Model):
+    doctor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    patient_id = models.IntegerField()
+    medication_id = models.IntegerField()
+    description = models.TextField()
+    dose = models.CharField(max_length=255)
+    prescription_file = models.FileField()
