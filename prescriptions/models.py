d38acf798311f463c4e3bf18d55d93b2092f8480
KarlaJulyana <karla@solarz.com.br>
2024-07-04 22:40:10 -0300

    chore: change doctor id field #2

--- prescriptions/models.py
+++ prescriptions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MedicalPrescription(models.Model):
-    doctor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor_id = models.IntegerField()
     patient_id = models.IntegerField()
     medication_id = models.IntegerField()
     description = models.TextField()
